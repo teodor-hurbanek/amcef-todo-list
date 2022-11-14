e9d569c92036ad21616b5464c5d8314dee936dd4
Teodor Hurbanek <teodorhurbanek@gmail.com>
2022-11-14 23:32:29 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-   // darkMode: "class",
-   // important: true, // important in prod is must be
-   // theme: ["dark"],
-   plugins: [require('daisyui')],
-   // daisyui: {
-   //   themes: [],
-   // },
-   // theme: {
-   //    extend: {},
-   // },
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('daisyui')],
+  daisyui: {
+    darkTheme: 'dark',
+    themes: ['dark'],
+  },
 }
